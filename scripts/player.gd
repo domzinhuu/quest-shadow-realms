b96b864d515ed301802411cc284886782233d31f
Maique Rosa <maique.rosa@gmail.com>
2026-02-13 09:34:05 -0300

    refactor: remove unused scaling function and clean up player movement logic
    - Eliminated the _apply_scaling function to streamline player scaling.
    - Cleaned up the _physics_process function for improved readability.

--- scripts/player.gd
+++ scripts/player.gd
@@ -41,30 +41,13 @@ func _physics_process(delta: float) -> void:
 	elif not stop_animations:
 		animation.play('idle')
 	
-	
-		
-
-			
 	move_and_slide()
 	_limit_player_area()
 	scale = Vector2(3,3)
-	#_apply_scaling()
 
 
 func _play_idle():
 	animation.play('idle')
-
-func _apply_scaling():
-	const MIN_Y: float = 0.0
-	const MAX_Y: float = 1080.0
-	
-	const MIN_SCALE: float = 1.5
-	const MAX_SCALE: float = 3.5
-	
-	var t: float = clamp((global_position.y - MIN_Y) / (MAX_Y - MIN_Y),0.0,1.0)
-	var new_scale: float = lerp(MIN_SCALE,MAX_SCALE,t)
-	
-	scale = Vector2(new_scale,new_scale)
 
 func _limit_player_area():
 	var min_x := 30.0
